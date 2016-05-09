e9f5fee74ad1e0dedacee2fddbc748d22ae78e39
Minhyun Yoo <ymh1989@korea.ac.kr>
2016-05-09 19:08:50 +0900

    Vectorization test (Monte Carlo simluation)

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Vectorization
-Computational cost in Monte Carlo simulation using vectorization
+## Computational cost in Monte Carlo simulation using vectorization ##
+
+###Introduction###
+This repo contains an implementation of pricing financial derivatives using Monte Calo simulation with vectorization calculation. Generally speaking, computer program spend most of the time in loop process. Especially, Monte Carlo simulation(MCS) in finance is mainly composed of `for-loop` or `while-loop`. 
+
+The main purpose of this repo is to introduce the method for increasing performance of MCS using vectorization calculation. The program is performed by Python. 
+
+### Methods ###
+As I mentioned in `Introduction`, `loop` is the main reason for increasing cpu time. It means that the more the number of `loop` and `nested-loop`, the more programs spend more time. One of role of `loop` is to handle vector or matrix type data. In order to improve the performance of vector and matrix operation, several libraries has been developed such as [Eigen-C++](http://eigen.tuxfamily.org/index.php?title=Main_Page), [Boost-C++](http://www.boost.org/), and [Numpy-Python](www.numpy.org/).
+
+
+###Environment###
+- CPU : Intel(R) Core(TM) i5-6400 @ 2.7GHZ 
+- RAM : DDR3L 16GB PC3-12800
+- [Python 2.7](https://www.python.org/), [numpy 1.10.4]()
+
+### Parameters ### 
+  | Stock | Strike | Maturity | Riskless <p>interest rate</p>  | Volatility | # of <p>simulations</p> 
+------------ | ------------- | ------------- | ------------- | -------------
+Parameters | 100.0 | 100.0 | 1.0 | 0.03 | 0.3 | 10<sup>5</sup> 
+
+###Result###
+- In this repo, I compare the cpu time(seconds) of three version for MCS. The parameters can be modified freely.
+- `version 0` : Call random number function every time
+- `version 1` : Call random number function at a time such as `(# of simulations × # of time steps)`
+- `version 2` : Vectorized path generation and call random number function at a time such as `(# of simulations × # of time steps)`
+
+cpu time(sec) | 10<sup>5</sup>simuls  | 10<sup>6</sup>simuls  | 10<sup>7</sup>simuls
+------------ | ------------- | ------------- | -------------
+version 0 | 0.1123 | 1.1427 | 11.3262
+version 1 | 0.1337 | 1.3427 | 13.1795
+version 2 | 0.0053 | 0.0749 | 0.7342
+** Exact value of European call option: 13.28331
+
+###Note###
+- If you're interested in my works, please visit my [homepage](https://sites.google.com/site/yoomh1989/).
---
+++ code/mc_vec_test.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+European call option by Monte Carlo simulation 
+
+test for vectorized calculation
+
+@author: Minhyun Yoo
+"""
+import time
+import numpy as np
+from math import exp, sqrt, log
+from scipy import stats
+def exc_call(S0, E, T, r, sig):
+    d1 = (log(S0/E) + (r + 0.5*sig**2)*T) / (sig*sqrt(T));
+    d2 = d1 - sig * sqrt(T);
+
+    call = ( S0 * stats.norm.cdf(d1, 0.0, 1.0) 
+        - E * exp(-r * T) * stats.norm.cdf(d2, 0.0, 1.0) )
+
+    print 'Exact call Price : %.5f\n' % call
+
+def mc_call(S0, E, T, r, sig, numSim, numStep):
+
+    dt = T / numStep; # time step
+    accum = 0.0; # accumulated payoff
+    t0 = time.clock();
+    for i in range(numSim): # simulation loop
+        s = S0;
+        for j in range(numStep): # timestep loop
+            z = np.random.normal();
+            # Geometric Brownian motion
+            s = s * exp((r - 0.5*sig**2)*dt + sig*sqrt(dt)*z);
+        payoff = max(s - E, 0); # vanilla call
+        accum = accum + payoff; # accumulate payoff
+    call = exp(-r * T) * (accum / numSim); # expectation
+    t1 = time.clock();
+    
+    print 'version 0' 
+    print 'Call Price : %.5f' % call
+    print 'CPU time in Python(sec) : %.4f' % (t1-t0)
+    
+def mc_call_vec1(S0, E, T, r, sig, numSim, numStep):
+    dt = T / numStep;
+    accum = 0.0;
+    t0 = time.clock();
+    z = np.random.normal(size = [numSim, numStep]);
+    for i in range(numSim):
+        s = S0;
+        for j in range(numStep):
+            s = s * exp((r - 0.5*sig**2)*dt + sig*sqrt(dt)*z[i, j]);
+        payoff = max(s - E, 0);
+        accum = accum + payoff;
+    call = exp(-r * T) * (accum / numSim);
+    t1 = time.clock();
+    
+    print 'version 1' 
+    print 'Call Price : %.5f' % call
+    print 'CPU time in Python(sec) : %.4f' % (t1-t0)
+    
+def mc_call_vec2(S0, E, T, r, sig, numSim, numStep):
+    dt = T / numStep;
+    t0 = time.clock();
+    z = np.random.normal(size = [numSim, numStep]);
+    s = S0 * np.ones([numSim]);
+    for j in range(numStep):
+        s[:] = s[:] * np.exp((r - 0.5*sig**2)*dt + sig*sqrt(dt)*z[:, j]);
+    payoff = np.maximum(s - E, 0);
+    call = exp(-r * T) * np.mean(payoff);
+    t1 = time.clock();
+    
+    print 'version 2' 
+    print 'Call Price : %.5f' % call
+    print 'CPU time in Python(sec) : %.4f' % (t1-t0  )
+
+S0 = 100.0; # underlying price
+E = 100.0; # strike price
+T = 1.0; # maturity
+r = 0.03; # riskless interest rate
+sig = 0.3; # volatility
+ns = 1000000;  # # of simulations
+nStep = 1; # # of time steps (In this example, nStep does not have to over 1 due to European option pricing.)
+
+# functions call
+exc_call(S0, E, T, r, sig);
+mc_call(S0, E, T, r, sig, ns, nStep);
+mc_call_vec1(S0, E, T, r, sig, ns, nStep);
+mc_call_vec2(S0, E, T, r, sig, ns, nStep);
+
